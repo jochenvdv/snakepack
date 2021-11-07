5fd1f15ac4049e83e5527d8e9a414dd3f8f4c7ee
Jochen Van de Velde <mail@jochenvandevelde.be>
2021-11-07 20:57:05 +0100

    Implement fully qualified Python name configuration type

--- snakepack/config/types.py
+++ snakepack/config/types.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import re
 import sys
 from enum import Enum, unique
+from typing import Sequence, Optional, Match, Union
 
 
 @unique
@@ -13,3 +15,62 @@ class PythonVersion(Enum):
     @classmethod
     def current(cls) -> PythonVersion:
         return PythonVersion(f'{sys.version_info[0]}.{sys.version_info[1]}')
+
+
+class FullyQualifiedPythonName(str):
+    _MODULE_NAME_REGEX = r'([a-z0-9_]+)(\.[a-z0-9_]+)*'
+    _IDENTIFIER_NAME_REGEX = r'([a-z_][a-z0-9_]*)(\.[a-z_][a-z0-9_]*)*'
+
+    _REGEX = re.compile(
+        rf'^(?P<module_path>{_MODULE_NAME_REGEX})(:(?P<ident_path>{_IDENTIFIER_NAME_REGEX}))?$'
+    )
+
+    def __new__(cls, value: Union[str, Match]):
+        match = cls._REGEX.fullmatch(value)
+
+        if not match:
+            raise ValueError('Invalid fully qualified python name')
+
+        module_path = match.group('module_path')
+        ident_path = match.group('ident_path')
+
+        mod_path_elems = []
+
+        if module_path is not None:
+            mod_path_elems.extend(module_path.split('.'))
+
+        id_path_elems = []
+
+        if ident_path is not None:
+            id_path_elems.extend(ident_path.split('.'))
+
+        obj = str.__new__(cls, value)
+
+        obj._module_path = mod_path_elems
+        obj._ident_path = id_path_elems
+
+        return obj
+
+    @property
+    def module_path(self) -> Sequence[str]:
+        return list(self._module_path)
+
+    @property
+    def ident_path(self) -> Sequence[str]:
+        return list(self._ident_path)
+
+    @property
+    def has_module_path(self) -> bool:
+        return len(self._module_path) > 0
+
+    @property
+    def has_ident_path(self) -> bool:
+        return len(self._ident_path) > 0
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value):
+        return cls(value)
--- tests/unit/config/test_types.py
+++ tests/unit/config/test_types.py
@@ -1,7 +1,9 @@
 import platform
 import sys
 
-from snakepack.config.types import PythonVersion
+import pytest
+
+from snakepack.config.types import PythonVersion, FullyQualifiedPythonName
 
 
 class PythonVersionTest:
@@ -21,3 +23,69 @@ class PythonVersionTest:
         assert isinstance(version, PythonVersion)
         assert version in PythonVersion
         assert platform.python_version().startswith(version.value)
+
+
+class FullyQualifiedPythonNameTest:
+    def test_full_nested(self):
+        fqn = FullyQualifiedPythonName('pkg.module:class.attr')
+
+        assert fqn.module_path == ['pkg', 'module']
+        assert fqn.ident_path == ['class', 'attr']
+        assert fqn.has_module_path
+        assert fqn.has_ident_path
+
+    def test_full_nested_deep(self):
+        fqn = FullyQualifiedPythonName('bigpkg.pkg.module:class.attr.anattr')
+
+        assert fqn.module_path == ['bigpkg', 'pkg', 'module']
+        assert fqn.ident_path == ['class', 'attr', 'anattr']
+        assert fqn.has_module_path
+        assert fqn.has_ident_path
+
+    def test_validate_full_shallow(self):
+        fqn = FullyQualifiedPythonName('module:class')
+
+        assert fqn.module_path == ['module']
+        assert fqn.ident_path == ['class']
+        assert fqn.has_module_path
+        assert fqn.has_ident_path
+
+    def test_validate_single_nested(self):
+        fqn = FullyQualifiedPythonName('pkg.module')
+
+        assert fqn.module_path == ['pkg', 'module']
+        assert fqn.ident_path == []
+        assert fqn.has_module_path
+        assert not fqn.has_ident_path
+
+    def test_validate_single_shallow(self):
+        fqn = FullyQualifiedPythonName('module')
+
+        assert fqn.module_path == ['module']
+        assert fqn.ident_path == []
+        assert fqn.has_module_path
+        assert not fqn.has_ident_path
+
+    def test_validate_single_wrong_nesting_punctuation(self):
+        with pytest.raises(ValueError):
+            fqn = FullyQualifiedPythonName('.module')
+
+    def test_validate_full_wrong_nesting_punctuation(self):
+        with pytest.raises(ValueError):
+            fqn = FullyQualifiedPythonName('module:.attr')
+
+    def test_validate_blocking_separator(self):
+        with pytest.raises(ValueError):
+            fqn = FullyQualifiedPythonName(':attr')
+
+    def test_validate_hanging_separator(self):
+        with pytest.raises(ValueError):
+            fqn = FullyQualifiedPythonName('module:')
+
+    def test_validate_empty(self):
+        with pytest.raises(ValueError):
+            fqn = FullyQualifiedPythonName('')
+
+    def test_validate_separator_only(self):
+        with pytest.raises(ValueError):
+            fqn = FullyQualifiedPythonName(':')
