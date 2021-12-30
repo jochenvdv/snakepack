04f33b19567ce621c915014c07a9cf5ff062966c
Jochen Van de Velde <mail@jochenvandevelde.be>
2021-12-30 11:51:35 +0100

    Testing: clean up collected performance metrics

--- conftest.py
+++ conftest.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 from pytest_harvest import is_main_process, get_xdist_worker_id, get_session_results_df
 
 
 def pytest_sessionfinish(session):
     session_results_df = get_session_results_df(session)
     suffix = 'all' if is_main_process(session) else get_xdist_worker_id(session)
+    session_results_df = _clean_result_data(session_results_df)
     session_results_df.to_csv('results_%s.csv' % suffix)
+
+
+def _clean_result_data(df):
+    return df.drop(columns=['pytest_obj'])
